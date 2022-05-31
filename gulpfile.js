58a31971a28a95c8d43af013538a0525d0ccebed
starlabelgh <hello@starlabel.co>
2022-05-31 21:33:19 +0000

    css sass updated

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,11 @@
 var gulp = require('gulp');
 var watch = require('gulp-watch');
+var postcss = require('gulp-postcss');
+var autoprefixer = require('autoprefixer');
+var cssvars = require('postcss-simple-vars'); 
+var nested = require('postcss-nested');
+var cssImport = require('postcss-import')
+
 
 gulp.task('default', function(){
     return gulp.src('package.json')
@@ -13,7 +19,9 @@ gulp.task('html', function(){
 
 gulp.task('styles', function(){
     return gulp.src('package.json')
-    console.log("Imagine Sass or PostCSS task running here");
+    return gulp.src('./app/assets/styles/styles.css')
+    .pipe(postcss([cssImport, cssvars, nested, autoprefixer]))
+    .pipe(gulp.dest('./app/temp/styles'));
 });
 
 gulp.task('watch', function(){
