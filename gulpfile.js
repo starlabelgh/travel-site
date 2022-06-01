95dc925e29f43dfb28c68d2467ea490ca208469e
starlabelgh <hello@starlabel.co>
2022-06-01 08:51:53 +0000

    updated gulp functions on the auto inject sync page

--- gulpfile.js
+++ gulpfile.js
@@ -4,8 +4,46 @@ var postcss = require('gulp-postcss');
 var autoprefixer = require('autoprefixer');
 var cssvars = require('postcss-simple-vars'); 
 var nested = require('postcss-nested');
-var cssImport = require('postcss-import')
+var cssImport = require('postcss-import');
+var browserSync = require('browser-sync').create();
 
+// Gulp update from stackoverflow code begin
+var gulp = require('gulp'),
+    watch = require('gulp-watch');
+    //watchLess = require('gulp-watch-less'),
+    //pug = require('gulp-pug'),
+    //less = require('gulp-less'),
+    //minifyCSS = require('gulp-csso'),
+    //concat = require('gulp-concat'),
+    //sourcemaps = require('gulp-sourcemaps');
+
+gulp.task('watch', function () {
+    gulp.watch('./app/temp/styles.css', ['css']);
+});
+
+gulp.task('html', function(){
+    return gulp.src('./app/index.html')
+    .pipe(pug())
+    .pipe(gulp.dest('./app/temp/html'))
+});
+
+gulp.task('css', function(){
+return gulp.src('./app/assets/styles/styles.css')
+    .pipe(less())
+    .pipe(minifyCSS())
+    .pipe(gulp.dest('./app/temp/styles'))
+});
+
+gulp.task('js', function(){
+    return gulp.src('./app/assets/styles/.js')
+    .pipe(sourcemaps.init())
+    .pipe(concat('app.min.js'))
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('./app/temp/js'))
+});
+
+gulp.task('default', [ 'html', 'js', 'css', 'watch']);
+// Gulp update from stackoverflow code end
 
 gulp.task('default', function(){
     return gulp.src('package.json')
@@ -26,12 +64,24 @@ gulp.task('styles', function(){
 
 gulp.task('watch', function(){
     return gulp.src('package.json')
+
+    browserSync.init({
+        server: {
+            baseDir: "app"
+        }
+    });
+
     watch('./app/index.html', function(){
-        gulp.start('html');
+        browserSync.reload();
     });
 
     watch('./app/assets/styles/**/*.css', function(){
-        gulp.start('styles');
+        gulp.start('cssInject');
     });
 })
+
+gulp.task('cssInject', ['styles'], function(){
+    gulp.src('.app/temp/styles/styles.css')
+    pipe(browserSync.stream());
+});
 
